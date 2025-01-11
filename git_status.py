adab18bcb7eb62d900596cb04926336425d6f864
Christian <c.ship.johnson@gmail.com>
2025-01-10 21:09:31 -0500

    Test Commit

--- git_status.py
+++ git_status.py
@@ -29,7 +29,7 @@ def run_git_command(path, is_wsl, command):
 def write_output(file_path, data):
     with open(file_path, "w") as f:
         f.write(data)
-
+ 
 # Main Logic
 if __name__ == "__main__":
     if not os.path.exists(config_file):
@@ -46,11 +46,8 @@ if __name__ == "__main__":
 
         if is_wsl or os.path.exists(path):
             local_changes = run_git_command(path, is_wsl, "status --porcelain")
-            print("Local Changes: " + local_changes)
             unpushed_commits = run_git_command(path, is_wsl, "log @{u}..")
-            print(unpushed_commits)
             unpulled_commits = run_git_command(path, is_wsl, "log ..@{u}")
-            print(unpulled_commits)
 
             output += f"{repo}:\n"
             output += f"  Local Changes: {len(local_changes.splitlines())}\n" if local_changes else "  Local Changes: 0\n"
